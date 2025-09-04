64c8d359058a9d19bd09e54466e63312382498cc
Mahipal Pawar <mahipawar3434@gmail.com>
2025-09-05 00:46:12 +0530

    react js form validation

--- client-side/src/components/AddProduct.js
+++ client-side/src/components/AddProduct.js
@@ -6,11 +6,18 @@ const AddProduct = () => {
     const [price, setPrice] = react.useState('');
     const [category, setCategory] = react.useState('');
     const [company, setCompany] = react.useState('');
+    const [error, setError] = react.useState(false);
 
     console.log(name, price, category, company);
-    
+
     const submitData = async () => {
-      let userId = JSON.parse(localStorage.getItem('user'))._id;
+
+        if (!name || !price || !category || !company) {
+            // alert("Please fill all the fields");
+            // return false;
+            setError(true);
+        }
+        let userId = JSON.parse(localStorage.getItem('user'))._id;
         let result = await fetch('http://localhost:5000/add-product', {
             method: 'post',
             body: JSON.stringify({ name, price, category, company, userId }),
@@ -24,10 +31,14 @@ const AddProduct = () => {
     return (
         <div className='register'>
             <div className="register_box">
-                <input className='inputBox' type="text" placeholder='Enter Product Name' onChange={(e)=> setName(e.target.value)} />
-                <input className='inputBox' type="text" placeholder='Enter Product Price' onChange={(e)=> setPrice(e.target.value)} />
-                <input className='inputBox' type="text" placeholder='Enter Product Category' onChange={(e)=> setCategory(e.target.value)} />
-                <input className='inputBox' type="text" placeholder='Enter Product Company'  onChange={(e)=> setCompany(e.target.value)}/>
+                <input className='inputBox' type="text" placeholder='Enter Product Name' onChange={(e) => setName(e.target.value)} />
+                {error && !name && <span className='invalid_input'>Enter valid name</span>}
+                <input className='inputBox' type="text" placeholder='Enter Product Price' onChange={(e) => setPrice(e.target.value)} />
+                {error && !price && <span className='invalid_input'>Enter valid price</span>}
+                <input className='inputBox' type="text" placeholder='Enter Product Category' onChange={(e) => setCategory(e.target.value)} />
+                {error && !category && <span className='invalid_input'>Enter valid category</span>}
+                <input className='inputBox' type="text" placeholder='Enter Product Company' onChange={(e) => setCompany(e.target.value)} />
+                {error && !company && <span className='invalid_input'>Enter valid company</span>}
                 <button className='signUp' onClick={submitData}>Add Product</button>
             </div>
         </div>
